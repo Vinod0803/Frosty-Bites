5ab7781b7a9b1970dce61478402e837a9bfee017
Vinod0803 <vinodchowdary449@gmail.com>
2026-03-23 14:19:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -440,4 +440,5 @@ res.send("Saved");
 );
 
 });
-app.listen(5000, ()=>console.log("Server running 🚀"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running 🚀"));
