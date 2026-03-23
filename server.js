54720ae3e6cd5d3fd0fd2a5a3bfe86c41ddb387e
Vinod0803 <vinodchowdary449@gmail.com>
2026-03-23 21:12:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,14 +48,7 @@ const razorpay = new Razorpay({
   key_secret: "xxxxxxxxxxxxxx"     // 👈 paste yours
 });
 // ✅ MySQL connection
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
-
+const db = mysql.createConnection(process.env.MYSQL_URL);
 db.connect(err => {
   if (err) {
     console.log("DB Error ❌", err);
