76a9090b2e1dcac4068cc6c8710ada89176b7b0c
Vinod0803 <vinodchowdary449@gmail.com>
2026-03-23 15:51:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,13 +48,7 @@ const razorpay = new Razorpay({
   key_secret: "xxxxxxxxxxxxxx"     // 👈 paste yours
 });
 // ✅ MySQL connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect(err => {
   if (err) {
